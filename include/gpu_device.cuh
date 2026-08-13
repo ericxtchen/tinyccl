bfeeddbc805bcfe467d01a4e6dc29216404fb1b2
ericxtchen <96218207+ericxtchen@users.noreply.github.com>
2026-08-12 21:37:36 -0400

    add gpu device with kernel function

---
+++ include/gpu_device.cuh
@@ -0,0 +1,33 @@
+#ifndef GPUDEVICE_H
+#define GPUDEVICE_H
+
+#include <cuda_runtime.h>
+
+template <typename T>
+__global__ void reduce_add_kernel(const T *src_buffer, T *dest_buffer,
+                                  std::size_t count) {
+  int id = blockIdx.x * blockDim.x + threadIdx.x;
+
+  if (id < count) {
+    dest_buffer[id] += src_buffer[id];
+  }
+}
+
+template <typename T> class GpuDevice {
+public:
+  void reduce_add(const T *src_buffer, T *dest_buffer, std::size_t count) {
+    if (count == 0)
+      return;
+
+    // Configure thread/block dimensions
+    int threads = 256;
+    int blocks = (count + threads - 1) / threads;
+
+    reduce_add_kernel<T><<<blocks, threads>>>(src_buffer, dest_buffer, count);
+
+    // Ensure the kernel finishes before returning
+    cudaDeviceSynchronize();
+  }
+};
+
+#endif
